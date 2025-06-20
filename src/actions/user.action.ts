37e3d25d186375626c5ead9f74ae2151d9ac8109
dparmar <116805076+dparmar04@users.noreply.github.com>
2025-06-20 16:25:07 +0530

    Create Post and Toggle Follow functionality

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import { prisma } from "@/lib/prisma";
 import { auth, currentUser } from "@clerk/nextjs/server";
+import { revalidatePath } from "next/cache";
 
 export async function syncUser() {
   try {
@@ -13,7 +14,7 @@ export async function syncUser() {
     //checks if user exists
     const existingUser = await prisma.user.findUnique({
       where: {
-        clearkId:userId
+        clerkId:userId
       }
     })
 
@@ -24,7 +25,7 @@ export async function syncUser() {
 
     const dbUser = await prisma.user.create({ 
       data: {
-        clearkId: userId,
+        clerkId: userId,
         name: `${user.firstName || ""} ${user.lastName || ""}`,
         username: user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
         email: user.emailAddresses[0].emailAddress,
@@ -35,5 +36,128 @@ export async function syncUser() {
     return dbUser;
   } catch (error) {
     console.log("Error in syncUser", error);
+  }
+}
+
+export async function getUserByClerkId(clerkId: string) {
+  return prisma.user.findUnique({
+    where: {
+      clerkId,
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true
+        }
+      }
+    }
+  })
+}
+
+export async function getDbUserId() {
+  const { userId: clerkId } = await auth()
+  if (!clerkId) throw new Error("Unauthorized");
+  
+  const user = await getUserByClerkId(clerkId);
+  if (!user) throw new Error("User Not Found");
+
+  return user.id
+}
+
+export async function getRandomUsers() {
+  try {
+    const userId = await getDbUserId();
+    
+
+    //get 3 random user exclude oursleves & users that we already follow
+    const randomUser = await prisma.user.findMany({
+      where: {
+        AND: [
+          {NOT: {id:userId}},
+          {
+            NOT: {
+              followers: {
+                some: {
+                  followerId: userId 
+                }
+              }
+            }
+          }
+        ]
+      },
+      select: {
+        id: true,
+        name: true,
+        username: true,
+        image: true,
+        _count: {
+          select: {
+            followers: true
+          },
+        }
+      },
+      take: 3,
+    })
+
+    return randomUser;
+  } catch (error) {
+    console.log("Error fetching random users", error);
+    return [];
+  }
+  
+}
+
+export async function toggleFollow(targetUserId:string) {
+  try {
+    const userId = await getDbUserId();
+
+    if (userId === targetUserId) throw new Error("You cannot follow yourself");
+
+    const existingFollow = await prisma.follows.findUnique({
+      where: {
+        followerId_followingId: {
+          followerId: userId,
+          followingId: targetUserId
+        }
+      }
+    })
+
+    if (existingFollow) {
+      //unfollow
+      await prisma.follows.delete({
+        where: {
+          followerId_followingId: {
+            followerId: userId,
+            followingId: targetUserId
+            
+          }
+        }
+      })
+    } else {
+      //follow
+      await prisma.$transaction([
+        prisma.follows.create({
+          data: {
+            followerId: userId,
+            followingId: targetUserId
+          }
+        }),
+        prisma.notification.create({
+          data: {
+            type: "FOLLOW",
+            userId: targetUserId, //user being followed
+            creatorId: userId //user following
+          }
+        })
+      ])
+    }
+
+    revalidatePath("/");
+    return{success:true}
+  } catch (error) {
+    console.log("Error in toggleFollow", error);
+    return{success: false, error:"Error toggling follow "}
   }
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,12 +43,15 @@ export default function RootLayout({
                 {/* Container to center the contetn */}
                 <div className="max-w-7xl mx-auto px-4">
                   <div className="grid grid-col-1 lg:grid-cols-12 gap-6">
-                    <div className="hidden lg:block lg:col-span-3">sidebar</div>
+                    <div className="hidden lg:block lg:col-span-3">
+                      <Sidebar />
+                    </div>
                     <div className="lg:col-span-9">{children}</div>
                   </div>
                 </div>
               </main>
             </div>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
